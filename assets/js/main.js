f3d89abded92b21929562a56b0bc484fa0afa069
heqingyang <heqingyang@meituan.com>
2016-03-15 15:11:51 +0800

    feat(js):finish js config page

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,34 +17,34 @@ app.directive('fullpage', function () {
 });
 // 滚动的页首位置
 app.directive('scrollTop', ['$rootScope',function ($rootScope) {
-   return {
-       restrict:'A',
-       scope:{},
-       link: function (scope,el) {
-           var scrollTopFlag = $(window).scrollTop() > 100;
-           $rootScope.showForkme = scrollTopFlag;
-           if (scrollTopFlag) {
-               el.fadeIn();
-           } else {
-               el.fadeOut();
+    return {
+        restrict:'A',
+        scope:{},
+        link: function (scope,el) {
+            var scrollTopFlag = $(window).scrollTop() > 100;
+            $rootScope.showForkme = scrollTopFlag;
+            if (scrollTopFlag) {
+                el.fadeIn();
+            } else {
+                el.fadeOut();
 
-           }
-           $(window).scroll(function () {
-               scrollTopFlag = $(this).scrollTop() > 100;
-               $rootScope.showForkme = scrollTopFlag;
-               $rootScope.$digest();
-               if (scrollTopFlag) {
-                   el.fadeIn();
-               } else {
-                   el.fadeOut();
-               }
-           });
-           el.click(function () {
-               $("html, body").animate({scrollTop: 0}, 600);
-               return false;
-           });
-       }
-   };
+            }
+            $(window).scroll(function () {
+                scrollTopFlag = $(this).scrollTop() > 100;
+                $rootScope.showForkme = scrollTopFlag;
+                $rootScope.$digest();
+                if (scrollTopFlag) {
+                    el.fadeIn();
+                } else {
+                    el.fadeOut();
+                }
+            });
+            el.click(function () {
+                $("html, body").animate({scrollTop: 0}, 600);
+                return false;
+            });
+        }
+    };
 }]);
 // 转化 select 绑定的数据为数字
 app.directive('convertToNumber',function() {
@@ -106,41 +106,126 @@ app.controller('appCtrl', ['$scope','$rootScope',function ($scope,$rootScope) {
     $rootScope.htmlRules = [];
     $rootScope.cssRules = [];
     $rootScope.jsRules = [];
+    $rootScope.disableStatus = {}
     // 禁用HTML检测的标记
-    $rootScope.disableHTML = false;
+    $rootScope.disableStatus.disableHTML = false;
     // 禁用CSS检测的标记
-    $rootScope.disableCSS = false;
+    $rootScope.disableStatus.disableCSS = false;
     // 禁用JS检测的标记
-    $rootScope.disableJS = false;
-    $scope.jsonConfig = '{}';
-    /* 假数据S */
-    var mockData = {
-        "html":{
-            "rules":{
-                "id-name-unique":false,"style-disabled":true,"inline-style-disabled":true,"doctype-first": false,"title-tag-require": false,"tagname-lowercase": true,"tag-pair": true,"attr-lowercase": false,"spec-char-escape": false
-            }
-        },
-        "css":{
-            "rules":{
-                "a-line-of-decl": {"level": 1},"after-colon": {"level": 1},"attribute-selector": {"level": 2},"before-colon": {"level": 1},"color-abbr": {"level": 1},"content-double-quotation": {"level": 1},"decl-comma": {"level": 1,"followedComma": " "},"decl-end-of-semicolon": {"level": 1},"disallow-import": {"level": 1},"disallow-named-color": {"level": 1},"disallow-quotes-in-url": {"level": 1},"disallow-use-expression": {"level": 1},"vendor-prefixes-sort": {"level": 1}
-            }
-        },
-        "js":{
-            "extends":"eslint:recommended",
-            "rules": {
-                "no-undef":2,"no-func-assign":0,"no-unexpected-multiline":2,"no-multi-str":[2],"no-sequences":[2],"no-unused-expressions":[2],"no-delete-var":[2],"no-undefined":[2],"no-unused-vars":[2],"no-use-before-define":[2,"nofunc"]
-            }
-        }
-    };
-    /* 假数据E */
 
-    $scope.downloadConfigHandler = function (evt) {
-        var json = mockData,
+    $rootScope.disableStatus.disableJS = false;
+    // 启用JS推荐配置的标记
+    $rootScope.jsRecommended = false;
+
+    $rootScope.makeConfig = function (evt) {
+        var lintConfig = {},
+            tempRules = {},
+            htmlConfig = {
+                "suffix": ['html']
+            },
+            cssConfig = {
+                "suffix": 'css',
+                "ignore": [],
+            },
+            jsConfig = {
+                "suffix": ['js', 'es', 'es6'],
+                "ignore": [],
+                "env": {
+                    "node": true,
+                    "mocha": true
+                },
+                "globals": {
+                    "angular": true,
+                    "jQuery": true,
+                    "define": true,
+                    "require": true
+                }
+            };
+        // htmlConfig生成
+        if ($rootScope.disableStatus.disableHTML) {
+            htmlConfig = false;
+        } else {
+            tempRules = {};
+            angular.forEach($rootScope.htmlRules, function (rule) {
+                if (rule.default) {
+                    if (!rule.enable) {
+                        tempRules[rule.id] = false;
+                    } else {
+                        if (rule.id == 'attr-value-quotes' && rule.selected == 'single') {
+                            tempRules[rule.id] = 'single';
+                        }
+                    }
+                } else {
+                    if (rule.enable) {
+                        tempRules[rule.id] = true;
+                        if (rule.id == 'id-class-value') {
+                            tempRules[rule.id] = rule.selected;
+                        }
+                    }
+                }
+            })
+            htmlConfig.rules = tempRules;
+        }
+
+
+        // cssConfig生成
+        if ($rootScope.disableStatus.disableCSS) {
+            cssConfig = false;
+        } else {
+            tempRules = {};
+            angular.forEach($rootScope.cssRules, function (rule) {
+                if (!rule.enable) {
+                    tempRules[rule.id] = {'level': 0};
+                }
+            })
+            cssConfig.rules = tempRules;
+        }
+
+        // jsConfig生成
+        if ($rootScope.disableStatus.disableJS) {
+            jsConfig = false;
+        } else {
+            tempRules = {};
+            if ($rootScope.jsRecommended) {
+                jsConfig['extends'] = 'eslint:recommended';
+            }
+            angular.forEach($rootScope.jsRules, function (rule) {
+                if (rule.isRecommended) {
+                    if (!rule.enable) {
+                        if ($rootScope.jsRecommended) {
+                            tempRules[rule.id] = 0;
+                        }
+                    } else {
+                        if (!$rootScope.jsRecommended) {
+                            tempRules[rule.id] = rule.hasOptions ? [2] : 2;
+                        }
+                    }
+                } else {
+                    if (rule.enable) {
+                        tempRules[rule.id] = rule.hasOptions ? [2] : 2;
+                    }
+                }
+            })
+            jsConfig.rules = tempRules;
+        }
+
+        // lintConfig生成
+        lintConfig = {
+            "html": htmlConfig,
+            "css": cssConfig,
+            "js": jsConfig
+        }
+        //console.log(lintConfig);
+        //$scope.downloadConfigHandler = function (evt) {
+        var json = lintConfig,
             el = angular.element(evt.target);
         var content = ['data:text/json;charset=utf-8,'];
         content.push(encodeURIComponent(JSON.stringify(json, null, 2)));
-        el.attr('href',content.join(''));
+        el.attr('href', content.join(''));
+        //}
+
     }
+
 }]);
 app.controller('htmlConfigCtrl', ['$scope','$rootScope','rulesServices',function ($scope,$rootScope,rulesServices) {
     $scope.selectAll = false;
@@ -242,6 +327,7 @@ app.controller('cssConfigCtrl', ['$scope','$rootScope','rulesServices',function 
 }]);
 app.controller('jsConfigCtrl', ['$scope','$rootScope','rulesServices',function ($scope,$rootScope,rulesServices) {
     $scope.selectAll = false;
+    $scope.selectRecommended = false;
     // 所有分类
     $scope.collections = [{
         label:'建议启用',
@@ -268,18 +354,37 @@ app.controller('jsConfigCtrl', ['$scope','$rootScope','rulesServices',function (
     // 把下列这些规则中配置的空格，转化为数字
     var rulesCache;
     rulesServices.getRules('conf/eslint.json').then(function (responese) {
-        //$rootScope.jsRules = responese.data;
-        //rulesCache = angular.copy(responese.data);
-        //angular.forEach($rootScope.jsRules, function (rule) {
-        //});
+        $rootScope.jsRules = responese.data;
+        rulesCache = angular.copy(responese.data);
+        angular.forEach($rootScope.jsRules, function (rule) {
+        });
     });
     // 全选
     $scope.selectAllHandler = function () {
+        var selectAll = $scope.selectAll;
+        angular.forEach($rootScope.jsRules, function (rule) {
+            rule.enable = selectAll;
+        });
     };
 
     //恢复默认
     $scope.resetToDefault = function () {
         if(rulesCache){
+            angular.forEach($rootScope.jsRules, function (rule) {
+                rule.enable = false;
+            });
+        }
+    };
+
+    //使用推荐配置
+    $scope.selectRecommendedHandler = function () {
+        var selectRecommended = $scope.selectRecommended;
+        $rootScope.jsRecommended = $scope.selectRecommended;
+        if(rulesCache){
+            $rootScope.jsRules = angular.copy(rulesCache);
+            angular.forEach($rootScope.jsRules, function (rule) {
+                rule.enable = !!rule.isRecommended&&selectRecommended;
+            });
         }
     };
 }]);
