b1e297529481d41e3b9132134bc674ba38326ca2
YangJiyuan <yjy972080142@gmail.com>
2016-05-11 17:05:24 +0800

    chore(lib):combine libs

---
+++ libComb.js
@@ -0,0 +1,16 @@
+var libs = [
+    'lib/jquery/jquery.min.js',
+    'lib/angular/angular.min.js',
+    'lib/jszip-3.0.0/jszip.min.js',
+    'lib/file-saver/file-saver.min.js',
+    'lib/highlight/highlight.pack.js',
+    'lib/angular-sanitize/angular-sanitize.min.js',
+    'lib/ui-fugu/ui-fugu.min.js'
+];
+var dist = 'assets/js/lib.js';
+var fs = require('fs');
+var content = '';
+libs.forEach(function (libpath) {
+    content += fs.readFileSync(libpath,'utf8') + '\n';
+});
+fs.writeFileSync(dist,content,'utf8');
